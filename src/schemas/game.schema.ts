db5e1b0caa0cd1032b13db600111a4d0e30b12b9
Sevada Grigoryan <sevada.grigoryan12@gmail.com>
2021-10-03 20:48:06 +0400

    chore: fix spacing issues

--- src/schemas/game.schema.ts
+++ src/schemas/game.schema.ts
@@ -3,29 +3,29 @@ import { Document, ObjectId } from 'mongoose';
 import * as mongoose from 'mongoose';
 import { Transform, Type } from 'class-transformer';
 import { Publisher } from './publisher.schema';
- 
+
 export type GameDocument = Game & Document;
- 
+
 @Schema()
 export class Game {
   @Transform(({ value }) => value.toString())
   _id: ObjectId;
- 
+
   @Prop({ required: true })
   title: string;
- 
+
   @Prop()
   price: number;
- 
+
   @Prop()
   tags: string[];
 
   @Prop()
   releaseDate: Date;
- 
+
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Publisher.name })
   @Type(() => Publisher)
   publisher: Publisher;
 }
- 
+
 export const GameSchema = SchemaFactory.createForClass(Game);
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose';
 export type ObjectId = mongoose.Schema.Types.ObjectId;
 
 export const calculatePastDate = (month: number): Date => {
-    const date = new Date();
-    date.setMonth(date.getMonth() - month);
-    return new Date(date);
-}
+  const date = new Date();
+  date.setMonth(date.getMonth() - month);
+  return new Date(date);
+};
